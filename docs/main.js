7be6e705d57d8053a72fc10112c1116adf55c304
to-hutohu <tohu.soy@gmail.com>
2017-01-04 00:03:35 +0900

    po

--- docs/main.js
+++ docs/main.js
@@ -156,7 +156,7 @@ function gameEnd(result){
   ctx.fillText(result, 0, 25);
   ctx.textAlign ='center';
   ctx.fillText('Tweet:Press Space!', screen.width/2, screen.height/2);
-  vat text = result + 'でクリア！！！';
+  var text = result + 'でクリア！！！';
   if(space){
     window.open('http://twitter.com/?status='+ text, '_blank');
     return;
